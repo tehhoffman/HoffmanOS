9237895156f8b890e129546c4204d40c7d4cb8f7
Robert Hoffman <robert@HOFFHOME.lan>
2026-01-20 22:48:48 -0600

    Updated Update.sh to point to new repo

--- dArkOS_Tools/Update.sh
+++ dArkOS_Tools/Update.sh
@@ -16,7 +16,7 @@ fi
 
 sudo timedatectl set-ntp 1
 
-LOCATION="https://raw.githubusercontent.com/christianhaitian/darkos-updates/master"
+LOCATION="https://raw.githubusercontent.com/tehhoffman/HoffmanOS-Updates/master"
 
 wget -t 3 -T 60 --no-check-certificate "$LOCATION"/LICENSE -O /dev/shm/LICENSE -a "$LOG_FILE"
 if [ $? -ne 0 ]; then
@@ -25,21 +25,50 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-wget -t 3 -T 60 --no-check-certificate "$LOCATION"/dArkOSUpdate.sh -O /home/ark/dArkOSUpdate.sh -a "$LOG_FILE" || sudo rm -f /home/ark/dArkOSUpdate.sh | tee -a "$LOG_FILE"
+# Best-effort "no updates" detection:
+# If the updates repo exposes a version marker (recommended: `.VERSION` containing the build date),
+# compare it against the device's local version and exit early if they match.
+LOCAL_VERSION=""
+REMOTE_VERSION=""
+
+if [ -f /home/ark/.config/.VERSION ]; then
+  LOCAL_VERSION="$(tr -d '\r\n' </home/ark/.config/.VERSION)"
+elif [ -f /usr/share/plymouth/themes/text.plymouth ]; then
+  # Fallback: use the title string shown on boot (ex: "HoffmanOS (YYYY.MM.DD)")
+  LOCAL_VERSION="$(grep -m 1 '^title=' /usr/share/plymouth/themes/text.plymouth | cut -d'=' -f2- | tr -d '\r\n')"
+fi
+
+wget -q -t 2 -T 20 --no-check-certificate "$LOCATION"/.VERSION -O /dev/shm/.VERSION_REMOTE -a "$LOG_FILE"
+if [ $? -eq 0 ]; then
+  REMOTE_VERSION="$(tr -d '\r\n' </dev/shm/.VERSION_REMOTE)"
+else
+  # Fallback: if `.VERSION` isn't hosted, try comparing the plymouth title (if the repo hosts it).
+  wget -q -t 2 -T 20 --no-check-certificate "$LOCATION"/text.plymouth -O /dev/shm/text.plymouth.remote -a "$LOG_FILE"
+  if [ $? -eq 0 ]; then
+    REMOTE_VERSION="$(grep -m 1 '^title=' /dev/shm/text.plymouth.remote | cut -d'=' -f2- | tr -d '\r\n')"
+  fi
+fi
+
+if [ -n "$LOCAL_VERSION" ] && [ -n "$REMOTE_VERSION" ] && [ "$LOCAL_VERSION" = "$REMOTE_VERSION" ]; then
+  sudo msgbox "No updates are currently available."
+  exit 0
+fi
+
+wget -t 3 -T 60 --no-check-certificate "$LOCATION"/HoffmanOSUpdate.sh -O /home/ark/HoffmanOSUpdate.sh -a "$LOG_FILE" || sudo rm -f /home/ark/HoffmanOSUpdate.sh | tee -a "$LOG_FILE"
 if [ $? -ne 0 ]; then
   sudo msgbox "Looks like OTA updating is currently down or your wifi or internet connection is not functioning correctly."
   printf "There was an error with attempting this update." | tee -a "$LOG_FILE"
   exit 1
 fi
 
-sudo chmod -v 777 /home/ark/dArkOSUpdate.sh | tee -a "$LOG_FILE"
-/home/ark/dArkOSUpdate.sh
+sudo chmod -v 777 /home/ark/HoffmanOSUpdate.sh | tee -a "$LOG_FILE"
+/home/ark/HoffmanOSUpdate.sh
 
 if [ $? -ne 187 ]; then
   sudo msgbox "There was an error with attempting this update.  Did you make sure to enable your wifi and connect to a wifi network?  If so, enable remote services in options and try to update again."
   printf "There was an error with attempting this update." | tee -a "$LOG_FILE"
-  if [ -f /home/ark/dArkOSUpdate.sh ]; then
-    rm /home/ark/dArkOSUpdate.sh
+  if [ -f /home/ark/HoffmanOSUpdate.sh ]; then
+    rm /home/ark/HoffmanOSUpdate.sh
   fi
 fi
 
